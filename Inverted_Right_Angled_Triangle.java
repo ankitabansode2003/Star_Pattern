824dc04b0a0cbe61df2c52b81a9e63755ebc033a
Ankita Bansode <bansodeankita2003@gmail.com>
2025-02-03 13:21:58 +0530

    Add files via upload

---
+++ Inverted_Right_Angled_Triangle.java
@@ -0,0 +1,15 @@
+package com.star;
+
+public class Inverted_Right_Angled_Triangle {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=4;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
